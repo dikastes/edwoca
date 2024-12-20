469736d9f00f0c9be26f9f9a1654e54500d94691
Matthias Richter <matthias.richter@slub-dresden.de>
2024-12-20 13:33:56 +0100

    Add work list view

--- edwoca/urls.py
+++ edwoca/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = []
+app_name='edwoca'
+urlpatterns = [
+        path('', views.IndexView.as_view(), name = 'index')
+    ]
--- edwoca/views.py
+++ edwoca/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Work
+from dmad_on_django.models import Status
 
 # Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'edwoca/index.html'
+    context_object_name = 'work_list'
+
+    def get_queryset(self):
+        return [
+                {
+                    'gnd_id' : work.gnd_id,
+                    'title' : work.titles.get(status=Status.PRIMARY)
+                } for
+                work in
+                Work.objects.all()
+            ]
