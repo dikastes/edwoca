1ee9206252355839c8c98ec0ac57c5a297b78abc
Matthias Richter <matthias.richter@slub-dresden.de>
2024-12-27 08:45:13 +0100

    Rewire views

--- edwoca/urls.py
+++ edwoca/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name='edwoca'
 urlpatterns = [
-        path('', views.IndexView.as_view(), name = 'index')
+        path('', views.IndexView.as_view(), name = 'index'),
+        path('works/<int:pk>', views.WorkUpdateView.as_view(), name = 'work_detail'),
+        path('works/update/<int:pk>', views.work_update, name = 'work_update')
     ]
--- edwoca/views.py
+++ edwoca/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Work
 from dmad_on_django.models import Status
-
-# Create your views here.
+from .forms import WorkTitleForm
 
 class IndexView(generic.ListView):
     template_name = 'edwoca/index.html'
@@ -12,9 +11,32 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return [
                 {
+                    'id' : work.id,
                     'gnd_id' : work.gnd_id,
                     'title' : work.titles.get(status=Status.PRIMARY)
                 } for
                 work in
                 Work.objects.all()
             ]
+
+class WorkUpdateView(generic.edit.UpdateView):
+    model = Work
+    fields = [
+            'work_catalog_number',
+            'gnd_id',
+            'history'
+        ]
+    template_name_suffix = '_update'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.object.titles.get(status=Status.PRIMARY).title
+        context['work_title_forms'] = [ WorkTitleForm(instance=title) for title in self.object.titles.all() ]
+        return context
+
+class WorkDetailView(generic.DetailView):
+    template_name = 'edwoca/work_detail.html'
+    model = Work
+
+def work_update(request, work_id):
+    work = get_object_or_404(Work, id = work_id)
