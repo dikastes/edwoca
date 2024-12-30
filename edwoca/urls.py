2ce99d39c4048ad8a1e3bbaaebbc51cc2e107f48
Matthias Richter <matthias.richter@slub-dresden.de>
2024-12-30 17:32:42 +0100

    Add person list json view for rest api

--- edwoca/urls.py
+++ edwoca/urls.py
@@ -6,5 +6,6 @@ app_name='edwoca'
 urlpatterns = [
         path('', views.IndexView.as_view(), name = 'index'),
         path('works/<int:pk>', views.WorkUpdateView.as_view(), name = 'work_detail'),
-        path('works/update/<int:pk>', views.work_update, name = 'work_update')
+        path('works/update/<int:pk>', views.work_update, name = 'work_update'),
+        path('persons', views.person_list, name = 'person_list'),
     ]
